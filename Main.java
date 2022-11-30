214db0103c8f1205c54fb38a22084f0472e58f12
Ubuntu <TRUTHANIKET@linux-gui.tail99f89.ts.net>
2022-11-30 05:33:35 +0000

    this is a small test

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+class Main{
+public static void main(String[] args){
+
+double val=(101+0)/3.0 ;
+System.out.println("The double value is : "+val);
+double laung=3.0e-6 *10000000.1;
+System.out.println("this is a long value for the expression 3.0e-6 *10000000.1 : "+ laung);
+int wan=24 & 35 ;
+System.out.println("the expression 24 & 35 is :"+wan);
+int taw=27<<5 ;
+System.out.println("27<<5 expression is : "+taw);
+boolean twe=(false && false)|| (true && true) ;
+System.out.println("the expression is :  "+twe);
+
+}
+
+
+
+}
---
+++ q2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class q2 {
+public static void main(String [] args){
+
+Scanner input=new Scanner(System.in);
+System.out.println("eneter the inch size");
+int inch=input.nextInt();
+double foot=inch/12.0;
+System.out.println("the eneterd size is :"+foot+" feet");
+
+
+
+
+
+}
+
+
+}
